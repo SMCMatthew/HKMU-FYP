7c9b4b48962c0e88bb1d2ac76fb06b7e889689a6
SMCMatthew <80394334+SMCMatthew@users.noreply.github.com>
2025-02-09 16:44:15 +0800

    Gameplay Update

--- Assets/CurrentObjectTracker.cs
+++ Assets/CurrentObjectTracker.cs
@@ -12,6 +12,11 @@ public class CurrentObjectTracker : MonoBehaviour
 
     public SnapInteractable snapInteractable;
 
+    // Gameplay element
+    public GameObject currentObject;
+    public GameObject requireObject;
+    public bool isCorrect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +26,21 @@ public class CurrentObjectTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check the cuurent object in the interactable and apply the value to currentObject
         if (snapInteractable.SelectingInteractorViews.Any())
         {
             var interactorData = snapInteractable.SelectingInteractorViews.First().Data as MonoBehaviour;
-            print("AAA: " + interactorData.gameObject.name);
+            print("AAA: " + interactorData.transform.parent.gameObject.name);
+            currentObject = interactorData.transform.parent.gameObject;
         }
 
+        // Change the boolean if the currentObject is same as the requireObject
+        if (currentObject == requireObject)
+        {
+            isCorrect = true;
+            print("correct object");
+        }
+        else isCorrect = false;
         //print("BBB: " + snapInteractable.SelectingInteractorViews.First().Data as MonoBehaviour.gameObject);
         //foreach (IInteractorView interactorView in snapInteractable.SelectingInteractorViews)
         //{
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,17 +5,34 @@ using Oculus.Interaction;
 
 public class GameManager : MonoBehaviour
 {
-    public SnapInteractable snapInteractable;
+    public GameObject[] gameObjects; // Array to hold references to the GameObjects
+    public GameObject[] requiredGameObjects; // Array to hold the required GameObjects
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize the GameObjects and Required GameObjects arrays
+        gameObjects = new GameObject[1];
+        requiredGameObjects = new GameObject[1];
+
+        //// Create and assign GameObjects
+        //for (int i = 0; i < gameObjects.Length; i++)
+        //{
+        //    // Instantiate your GameObjects and assign them
+        //    gameObjects[i] = new GameObject("GameObject" + (i + 1)).AddComponent<GameObjectVariable>();
+        //    requiredGameObjects[i] = new GameObject("RequiredObject" + (i + 1)); // Create required GameObjects
+
+        //    // Assign the required GameObject to the GameObjectVariable
+        //    gameObjects[i].requiredGameObject = requiredGameObjects[i];
+        //}
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        print("Array" + EqualityOperator(gameObjects, requiredGameObjects));
+    }
+
+    public bool EqualityOperator(GameObject[] firstArray, GameObject[] secondArray)
+    {
+        return firstArray == secondArray;
     }
 }
